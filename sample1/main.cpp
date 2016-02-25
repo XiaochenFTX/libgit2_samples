75697063d60bdc7c78418012f2c6ee8ed5bb539f
XiaochenFTX <xiaochenby706@sina.com>
2016-02-26 00:19:51 +0800

    repository init

--- sample1/main.cpp
+++ sample1/main.cpp
@@ -1,9 +1,24 @@
 #include <git2.h>
 
+#include <iostream>
+
 int main() {
+
+    const char* path = "../rep";
 
     git_libgit2_init();
 
+    git_repository* rep = nullptr;
+    int error = git_repository_init(&rep, path, 0);
+    if (error < 0) {
+        const git_error *e = giterr_last();
+        std::cout << "Error: " << error << "/" << e->klass << " : " << e->message << std::endl;
+
+        goto SHUTDOWN;
+    }
+
+SHUTDOWN:
+    git_repository_free(rep);
     git_libgit2_shutdown();
 
     return 0;
